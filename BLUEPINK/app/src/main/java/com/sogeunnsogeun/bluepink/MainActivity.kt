5b4ffa554196b0db330debd6aa493f7138bf86be
HyezNee <46377695+HyezNee@users.noreply.github.com>
2021-02-19 07:21:46 +0900

    중의적 의도 버튼 구현 완료, 심리 검사 버튼 구현

--- BLUEPINK/app/src/main/java/com/sogeunnsogeun/bluepink/MainActivity.kt
+++ BLUEPINK/app/src/main/java/com/sogeunnsogeun/bluepink/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
@@ -29,6 +30,8 @@ class MainActivity : AppCompatActivity() {
     private var msgList = listOf<MessageItem>()
     var btnCnt = 0
     var arr = mutableListOf<Button>()
+    var arr2 = mutableListOf<Button>()
+    var call_flow = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -117,11 +120,11 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    public fun chatWithBot(message:String){
-        val str:String = "{\"input_sentence\" : \"" + message + "\"}"
+    fun callApibyIntent(intent_id :String) {
+        val str:String = "{\"input_sentence\" : \"\", "
 
         val url: String = "https://danbee.ai/chatflow/chatbot/v2.0/7bce08fc-8747-4077-9c72-2d714d3c0e3a/message.do"
-        Thread({
+        Thread {
             URL(url)
                 .openConnection()
                 .let{
@@ -132,6 +135,8 @@ class MainActivity : AppCompatActivity() {
                     doOutput = true
                     val outputWriter = OutputStreamWriter(outputStream)
                     outputWriter.write(str)
+                    outputWriter.write("\"intent_id\" : \"$intent_id\", ")
+                    outputWriter.write("\"session_id\" : \"22420259\"}")
                     outputWriter.flush()
 
                 }.let {
@@ -147,11 +152,75 @@ class MainActivity : AppCompatActivity() {
                         }
                         it.close()
                         var temp = response.toString()
+                        Log.i("응답", temp);
+                        runOnUiThread{
+                            var result1 = JSONObject(JSONObject(temp).getJSONObject("responseSet").getJSONObject("result").getJSONArray("result").get(0).toString())
+                            var resultMsg = result1.get("message").toString()
+                            dialog_TV.text = "\" $resultMsg \""
+                        }
+                    }
+                }
+        }.start()
+    }
+
+    fun callApibyType(result :JSONObject) {
+        var type = result["type"].toString()
+        lateinit var input_message :String
+        if (type == "btn")
+            input_message = JSONObject(result.toString())["value"].toString()
+        else
+            input_message = ""
+        val str:String = "{\"input_sentence\" : \"$input_message\", "
+
+        val url: String = "https://danbee.ai/chatflow/chatbot/v2.0/7bce08fc-8747-4077-9c72-2d714d3c0e3a/message.do"
+        Thread {
+            URL(url)
+                .openConnection()
+                .let{
+                    it as HttpURLConnection
+                }.apply{
+                    setRequestProperty("Content-Type", "application/json;charset=UTF-8")
+                    requestMethod = "POST"
+                    doOutput = true
+                    val outputWriter = OutputStreamWriter(outputStream)
+                    outputWriter.write(str)
+                    outputWriter.write("\"session_id\" : \"22420259\", ")
+                    if (type == "callFlow") {
+                        var value = result["value"].toString()
+                        outputWriter.write("\"intent_id\" : \"$value\"}")
+                        call_flow = value
+                    }
+                    else if (type == "callNode") {
+                        var value = result["value"].toString()
+                        var idx = value.indexOf(".")
+                        value = value.substring(idx + 3)
+                        Log.i("노드", value)
+                        //outputWriter.write("\"intent_id\" : \"$call_flow\", ")
+                        outputWriter.write("\"node_id\" : \"$value\"}")
+                        //outputWriter.write("\"param_id\" : \"dummy\"}")
+                    }
+                    outputWriter.flush()
+
+                }.let {
+                    if (it.responseCode == 200) it.inputStream else it.errorStream
+                }.let { streamToRead ->
+                    BufferedReader(InputStreamReader(streamToRead)).use {
+                        val response = StringBuffer()
+
+                        var inputLine = it.readLine()
+                        while (inputLine != null) {
+                            response.append(inputLine)
+                            inputLine = it.readLine()
+                        }
+                        it.close()
+                        var temp = response.toString()
+                        Log.i("응답", temp);
                         runOnUiThread{
                             var result1 = JSONObject(JSONObject(temp).getJSONObject("responseSet").getJSONObject("result").getJSONArray("result").get(0).toString())
                             var resultMsg = result1.get("message").toString()
                             dialog_TV.text = "\" $resultMsg \""
                             val optionList = JSONArray(result1.getJSONArray("optionList").toString())
+                            // 선택지 버튼
                             btnCnt = optionList.length()
                             for (i in 0..optionList.length() - 1){
                                 var newBtn: Button = Button(this)
@@ -161,8 +230,96 @@ class MainActivity : AppCompatActivity() {
 
                                 dialog_window.addView(newBtn)
                                 arr.add(newBtn)
-                            }
 
+                                newBtn.setOnClickListener {
+                                    arr.forEach{
+                                        dialog_window.removeView(it)
+                                    }
+                                    arr.clear()
+                                    callApibyType(JSONObject(optionList[i].toString()))
+                                }
+                            }
+                        }
+                    }
+                }
+        }.start()
+    }
+
+    public fun chatWithBot(message:String){
+        val str:String = "{\"input_sentence\" : \"" + message + "\", "
+
+        val url: String = "https://danbee.ai/chatflow/chatbot/v2.0/7bce08fc-8747-4077-9c72-2d714d3c0e3a/message.do"
+        Thread {
+            URL(url)
+                .openConnection()
+                .let{
+                    it as HttpURLConnection
+                }.apply{
+                    setRequestProperty("Content-Type", "application/json;charset=UTF-8")
+                    requestMethod = "POST"
+                    doOutput = true
+                    val outputWriter = OutputStreamWriter(outputStream)
+                    outputWriter.write(str)
+                    outputWriter.write("\"session_id\" : \"22420259\"}")
+                    outputWriter.flush()
+
+                }.let {
+                    if (it.responseCode == 200) it.inputStream else it.errorStream
+                }.let { streamToRead ->
+                    BufferedReader(InputStreamReader(streamToRead)).use {
+                        val response = StringBuffer()
+
+                        var inputLine = it.readLine()
+                        while (inputLine != null) {
+                            response.append(inputLine)
+                            inputLine = it.readLine()
+                        }
+                        it.close()
+                        var temp = response.toString()
+                        Log.i("응답", temp);
+                        runOnUiThread{
+                            var result1 = JSONObject(JSONObject(temp).getJSONObject("responseSet").getJSONObject("result").getJSONArray("result").get(0).toString())
+                            var resultMsg = result1.get("message").toString()
+                            dialog_TV.text = "\" $resultMsg \""
+                            val optionList = JSONArray(result1.getJSONArray("optionList").toString())
+                            // 선택지 버튼
+                            btnCnt = optionList.length()
+                            for (i in 0..optionList.length() - 1){
+                                var newBtn: Button = Button(this)
+                                newBtn.layoutParams = ViewGroup.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+                                newBtn.text = JSONObject(optionList[i].toString())["label"].toString()
+                                newBtn.setId(i)
+
+                                dialog_window.addView(newBtn)
+                                arr.add(newBtn)
+
+                                newBtn.setOnClickListener {
+                                    arr.forEach{
+                                        dialog_window.removeView(it)
+                                    }
+                                    arr.clear()
+                                    callApibyType(JSONObject(optionList[i].toString()))
+                                }
+                            }
+                            //중의적 표현 버튼
+                            var another_result = JSONArray(JSONObject(temp).getJSONObject("responseSet").getJSONObject("result").getJSONArray("another_result").toString())
+                            for (i in 0..another_result.length() - 1){
+                                var arBtn: Button = Button(this)
+                                arBtn.layoutParams = ViewGroup.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+                                arBtn.text = JSONObject(another_result[i].toString())["intent_name"].toString()
+                                arBtn.setId(i)
+
+                                dialog_window.addView(arBtn)
+                                arr2.add(arBtn)
+
+                                arBtn.setOnClickListener {
+                                    arr2.forEach{
+                                        dialog_window.removeView(it)
+                                    }
+                                    arr2.clear()
+                                    callApibyIntent(JSONObject(another_result[i].toString())["intent_id"].toString())
+                                }
+                            }
 
                             //DB 추가
                             val addRunnable = Runnable{
@@ -178,11 +335,10 @@ class MainActivity : AppCompatActivity() {
                             addThread.start()
 
 
-
                         }
                     }
                 }
-        }).start()
+        }.start()
 
 
 
